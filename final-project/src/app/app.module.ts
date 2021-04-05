45bb2ce46e34b0f14f74fc95aa67f206eb5f1f63
bekatev <74668742+bekatev@users.noreply.github.com>
2021-04-05 22:21:16 +0400

    added add-hotel

--- final-project/src/app/app.module.ts
+++ final-project/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddHotelComponent } from './add-hotel/add-hotel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddHotelComponent
   ],
   imports: [
     BrowserModule,
