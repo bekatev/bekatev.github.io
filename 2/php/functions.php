b7feed03d9e33b91689ca7d3dec80f3eae579a84
bekatev <74668742+bekatev@users.noreply.github.com>
2021-04-05 21:29:37 +0400

    updated

--- 2/php/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'config.php';
-
-function register($db, $name, $score){
-    $new_user = "INSERT INTO users (usersName, usersScore) VALUES ('$name', '$score');";
-    mysqli_query($db, $new_user);
-}
